78385a2b92c75001f76f46ce6025853ca84190b0
herpin-dominique <117769343+herpin-dominique@users.noreply.github.com>
2023-09-14 23:47:43 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/reservations">Reservations</a>
+</nav>
+<p>avant</p>
+<slot />
+<p>after</p>
---
+++ src/routes/reservations/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+	<title>reservations</title>
+</svelte:head>
